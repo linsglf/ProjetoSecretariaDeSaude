3eaa758fd7e57020059263a945718ac2516cff90
Gabriel Lins <gabriellf.glf@gmail.com>
2023-10-07 05:18:35 -0300

    Introduces Municipio List Editing

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("/gui/primaryScene.fxml"));
+			Parent parent = FXMLLoader.load(getClass().getResource("/gui/PrimaryScene.fxml"));
 			Scene scene = new Scene(parent);
 			stage.setTitle("CRUD");
 			stage.setScene(scene);
